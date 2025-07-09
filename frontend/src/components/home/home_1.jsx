1ecdcc086cec4d5f9a36d5ae1532e1dd99fb2e34
anthonio-alex-dcosta <alexanthonio04@gmail.com>
2025-07-09 15:29:55 +0600

    Reuploading

---
+++ frontend/src/components/home/home_1.jsx
@@ -0,0 +1,52 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { useParams,  useNavigate } from 'react-router-dom';
+
+const EmailVerification = () => {
+    const { token } = useParams();
+    const navigate = useNavigate()
+    
+
+    const [message, setMessage] = useState('');
+    const [email, setEmail] = useState('');
+
+    useEffect(() => {
+        const verifyEmail = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3500/verify/${token}`);
+                setMessage(response.data.message);
+                localStorage.setItem('token', response.data.token);
+                navigate("/Home")
+                console.log('Email verified:', response.data.message);
+                console.log(email)
+            } catch (error) {
+                setMessage(error.response.data.message || 'An error occurred');
+                console.error('Error during email verification:', error);
+            }
+        };
+
+        verifyEmail();
+    }, [token]);
+
+    return (
+        <div            style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '30vh',
+            textAlign: 'center',
+        }}
+    >
+            <h1>Email Verification</h1>
+            <p>{message}</p>
+            <button     style={{
+                        marginTop: '40px',
+ 
+                         // Add more gap above the button
+                    }}>Edit Profile</button>
+        </div>
+    );
+};
+
+export default EmailVerification;
---
+++ frontend/src/components/home/home_before.jsx
@@ -0,0 +1,47 @@
+
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import "./home_before.css"
+
+
+
+const Initial = () => {
+
+  const navigate = useNavigate()
+  const userToken = localStorage.getItem('user_token')
+
+  useEffect(() => {
+    if (userToken) {
+      navigate('/Home')
+
+    }
+
+  } , [userToken, navigate])
+  return (
+
+ <body class="header">
+ <header>
+  <nav class="nav">
+    <div class="logo">Storygram</div>
+    <div class="menu">
+      <a>Home</a>
+      <a>Genres</a>
+      <a>My library</a>
+    </div>
+    <div id='buttons'>
+    <Link to={'/sign_in'}>
+      <button class='button'>Sign in</button>
+      
+    </Link>
+    <Link to={'/register'}>
+      <button class='button'>Sign up</button>
+    </Link>
+    </div>
+  </nav>
+
+ </header>
+ </body> 
+ )}
+
+export default Initial;
